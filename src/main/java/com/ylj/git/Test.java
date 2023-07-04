ae3a8b40924b0e42b81b6ca1f0fc06d139327500
ylj1210 <longquan1210@foxmail.com>
2023-07-04 13:02:21 +0800

    Initial commit

---
+++ src/main/java/com/ylj/git/Test.java
@@ -0,0 +1,13 @@
+package com.ylj.git;
+
+/**
+ * @title: Test
+ * @Author YuLianJie
+ * @Date: 2023/7/4 13:01
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
