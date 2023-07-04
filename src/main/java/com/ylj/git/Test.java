456ee7c5c0dee659753b8693a978e3a111257dc9
ylj1210 <longquan1210@foxmail.com>
2023-07-04 13:03:15 +0800

    111

--- src/main/java/com/ylj/git/Test.java
+++ src/main/java/com/ylj/git/Test.java
@@ -9,5 +9,7 @@ package com.ylj.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
